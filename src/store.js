f8bd093c1d76eee734be66ed639bdb3cea5826f8
6hislain <ghislainmike80@gmail.com>
2022-07-22 13:29:57 +0200

    drag and drop tasks

--- src/store.js
+++ src/store.js
@@ -1,14 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import defaultBoard from './default-board'
+import { saveStatePlugin, uuid } from './utils'
 
 Vue.use(Vuex)
 
 const board = JSON.parse(localStorage.getItem('board')) || defaultBoard
 
 export default new Vuex.Store({
-  state: {
-    board
+  plugins: [saveStatePlugin],
+  state: { board },
+  mutations: {
+    CREATE_TASK (state, { tasks, name }) {
+      tasks.push({ name, id: uuid(), description: '' })
+    },
+    UPDATE_TASK (state, { task, key, value }) {
+      Vue.set(task, key, value)
+    },
+    MOVE_TASK (state, { fromTasks, toTasks, taskIndex }) {
+      const taskToMove = fromTasks.splice(taskIndex, 1)[0]
+      toTasks.push(taskToMove)
+    }
   },
-  mutations: {}
+  getters: {
+    getTask (state) {
+      return (id) => {
+        for (const column of state.board.columns) {
+          for (const task of column.tasks) {
+            if (task.id === id) return task
+          }
+        }
+      }
+    }
+  }
 })
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,12 +1,63 @@
 <template>
   <div class="board">
-
+    <div class="flex flex-row items-start">
+      <div class="column" v-for="(column, $columnIndex) of board.columns" :key="$columnIndex"
+        @drop="moveTask($event, column.tasks)" @dragover.prevent @dragenter.prevent>
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div class="task" v-for="(task, $taskIndex) of column.tasks" :key="$taskIndex" @click="goToTask(task)"
+            @dragstart="pickupTask($event, $taskIndex, $columnIndex)" draggable>
+            <span class="w-full flext-no-shrink font-bold">
+              {{ task.name }}
+            </span>
+            <p v-if='task.description' class="w-full flex-no-shrink mt-1 text-sm">{{ task.description }}</p>
+          </div>
+          <input type="text" class="block p-2 w-full bg-transparent" placeholder="+ Enter new task"
+            @keyup.enter="createTask($event, column.tasks)" />
+        </div>
+      </div>
+    </div>
+    <div class="task-bg" v-if="isTaskOpen" @click.self="close">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex'
 
+export default {
+  computed: {
+    ...mapState(['board']),
+    isTaskOpen () {
+      return this.$route.name === 'task'
+    }
+  },
+  methods: {
+    goToTask (task) {
+      this.$router.push({ name: 'task', params: { id: task.id } })
+    },
+    close () { this.$router.push({ name: 'board' }) },
+    createTask (e, tasks) {
+      this.$store.commit('CREATE_TASK', { tasks, name: e.target.value })
+      e.target.value = ''
+    },
+    pickupTask (e, taskIndex, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.dropEffect = 'move'
+      e.dataTransfer.setData('task-index', taskIndex)
+      e.dataTransfer.setData('from-column-index', fromColumnIndex)
+    },
+    moveTask (e, toTasks) {
+      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
+      const fromTasks = this.board.columns[fromColumnIndex].tasks
+      const taskIndex = e.dataTransfer.getData('task-index')
+
+      this.$store.commit('MOVE_TASK', { fromTasks, toTasks, taskIndex })
+    }
+  }
 }
 </script>
 
@@ -26,6 +77,6 @@ export default {
 
 .task-bg {
   @apply pin absolute;
-  background: rgba(0,0,0,0.5);
+  background: rgba(0, 0, 0, 0.5);
 }
 </style>
